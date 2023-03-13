481351a6dd15739919ee460526dfdd29d3238748
arunit22 <arun.it.trainer@gmail.com>
2023-03-13 09:54:22 +0530

    Modified the code in Program.cs

--- AuthenticationExample/Program.cs
+++ AuthenticationExample/Program.cs
@@ -35,16 +35,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else
-{
-    // app.UseStatusCodePages();
-    //app.UseStatusCodePagesWithRedirects("/Error/{0}");
-    app.UseStatusCodePagesWithReExecute("/Error/{0}");
-}
+//else
+//{
+//    // app.UseStatusCodePages();
+//    //app.UseStatusCodePagesWithRedirects("/Error/{0}");
+//    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
